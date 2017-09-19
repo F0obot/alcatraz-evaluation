7ab9975adb92f39c9500ed32410a07cd7970d465
Christopher Thomas <christopher.thomas.333@gmail.com>
2017-09-19 12:57:40 -0500

    add new items

---
+++ src/pages/camera/camera.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
+
+/**
+ * Generated class for the CameraPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-camera',
+  templateUrl: 'camera.html',
+})
+export class CameraPage {
+  base64Image:any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public camera:Camera) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CameraPage');
+  }
+
+accessGallery(){
+  this.camera.getPicture({
+    sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+    destinationType: this.camera.DestinationType.DATA_URL
+  }).then((imageData) => {
+    this.base64Image = 'data:image/jpec;base64' + imageData;
+  }, (err) => {
+    console.log(err);
+  });
+}
+
+}
