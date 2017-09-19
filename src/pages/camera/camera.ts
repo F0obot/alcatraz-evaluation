15a62e8b2fda3c69d33348ca54e086b6ab319b4c
Christopher Thomas <christopher.thomas.333@gmail.com>
2017-09-19 14:19:54 -0500

    updating some things

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -14,24 +14,24 @@ import { Camera } from '@ionic-native/camera';
   templateUrl: 'camera.html',
 })
 export class CameraPage {
-  base64Image:any;
+  base64Image: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public camera:Camera) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public camera: Camera) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CameraPage');
   }
 
-accessGallery(){
-  this.camera.getPicture({
-    sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
-    destinationType: this.camera.DestinationType.DATA_URL
-  }).then((imageData) => {
-    this.base64Image = 'data:image/jpec;base64' + imageData;
-  }, (err) => {
-    console.log(err);
-  });
-}
+  accessGallery() {
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+      destinationType: this.camera.DestinationType.DATA_URL
+    }).then((imageData) => {
+      this.base64Image = 'data:image/jpeg;base64' + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
 }
