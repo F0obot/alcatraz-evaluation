dfd09b02320ff40df58b735724f181bafe96bbd1
Christopher Thomas <christopher.thomas.333@gmail.com>
2017-09-20 16:02:57 -0500

    fixing contacts

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
 import { Contacts } from '@ionic-native/contacts';
 
 
@@ -9,13 +10,27 @@ import { Contacts } from '@ionic-native/contacts';
 export class HelloIonicPage {
   contactsfound = [];
 
-  constructor(private contacts: Contacts) {
-    this.contacts = contacts;
+  constructor(private contacts: Contacts,
+    private toastCtrl: ToastController) {
   }
 
-  seeContacts(){
-    this.contacts.find(["displayName", "phoneNumbers"], {multiple: true}).then((contacts) => {
+  seeContacts() {
+    this.contacts.find(["displayName", "phoneNumbers"], { multiple: true }).then((contacts) => {
       this.contactsfound = contacts;
-     })
+      if (contacts != null && contacts.length < 1) {
+        let toast = this.toastCtrl.create({
+          message: 'Looks like you dont have any contacts :(',
+          duration: 3000,
+          position: 'bottom'
+        });
+
+        toast.onDidDismiss(() => {
+          console.log('Dismissed toast');
+        });
+
+        toast.present();
+
+      }
+    })
   }
 }
